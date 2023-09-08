db52c82ef8e8917ae08f8ba18c32dd11c71a4073
EC2 Default User <ec2-user@ip-10-0-0-99.ap-northeast-1.compute.internal>
2023-09-08 11:50:12 +0000

    todo_app

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import {useState,useEffect,useRef} from "react";
 import { Card } from './components/Card';
+import { v4 as uuidv4 } from 'uuid';
 //import { Routes, Route, Link } from 'react-router-dom';
 
 //import AddTodo from "./AddTodo";
@@ -20,30 +21,44 @@ export const App = () => {
   console.log("レンダリング")
   const[isAdmin,setIsAdmin] = useState(false);
   const [num,setNum] = useState(0);
-  const [todos,settodos] = useState([{id: 1,
-                                 name: "Todo1", completed: false}]);
-  const todoNameRef=useRef();
+  const [todos,setTodos] = useState([]);
+  const todoNameRef = useRef();//文字列情報習得
   
-  const handleAddTodo =()=>{
+  const handleAddTodo = () => {
     //タスクを追加
-    
-  };
+    const name = todoNameRef.current.value;
+    setTodos((prevTodos) => {
+      return [...prevTodos, {id: uuidv4(), name: name, completed: false }];
+    });
+      todoNameRef.current.value = null;
+   };
+   const toggleTodo = (id) =>{
+     const newTodos =[...todos];
+     const todo = newTodos.find((todo) => todo.id === id);
+     todo.completed =!todo.completed;
+     setTodos(newTodos);
+   };
+   
   const onClickBuntton =() =>{
       setNum(num + 1);
     };
      useEffect(() => {
     alert();
   },[num]);
+  const handleClear =() =>{
+    const newTodos =todos.filter((todo) => !todo.completed);
+    setTodos(newTodos);
+  }
   
   //const onClickSwitch =()=> setIsAdmin(!isAdmin);
   return (
     <>
     <div>
-     <TodoList todos={todos} />
-     <input type="text" ref={todoNameRef} />
+     <TodoList todos={todos} toggleTodo = {toggleTodo}/>
+     <input type="text" ref={todoNameRef} /> 　
      <button onClick={handleAddTodo}>タスクを追加</button>
-     <button>完了したタスクの削除</button>
-     <div>残りタスク：０</div>
+     <button onClick ={handleClear}>完了したタスクの削除</button>
+     <div>残りタスク：{todos.filter((todo)=> !todo.completed).length}</div>
       {isAdmin ? <span>管理者です。</span>:<span>管理者以外です。</span>}
       <button onClick={onClickBuntton}>切り替え</button>
       <Card isAdmin ={isAdmin} />
--- src/TodoList.js
+++ src/TodoList.js
@@ -2,12 +2,13 @@ import React from 'react';
 import Todo from "./Todo";
 
 
-const TodoList = ({ todos }) => {
-  if (!todos || !Array.isArray(todos)) {
-    return null; // or return some default UI when todos is undefined or not an array
-  }
 
-  return todos.map((todo) => <Todo key={todo} todo={todo} />);
+const TodoList = ({ todos, toggleTodo }) => {
+  //if (!todos || !Array.isArray(todos)) {
+  //  return null; // or return some default UI when todos is undefined or not an array
+//  }
+
+　return todos.map((todo) => <Todo todo={todo}key={todo.id} toggleTodo = {toggleTodo} />);//map関数配列で表示
 };
 
 export default TodoList;
